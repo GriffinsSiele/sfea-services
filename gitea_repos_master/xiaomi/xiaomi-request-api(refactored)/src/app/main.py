48c780f5f7371a4875b4ad6b1ec22c02c93469d8
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2025-09-23 10:13:48 +0400

    Update main.py

--- gitea_repos_master/xiaomi/xiaomi-request-api(refactored)/src/app/main.py
+++ gitea_repos_master/xiaomi/xiaomi-request-api(refactored)/src/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.api.v1.controllers import api_router
+from app.api.v1.aggregator import router as aggregator_router
 from app.middleware import add_middlewares
 
 
@@ -8,6 +9,7 @@ def create_app() -> FastAPI:
     app = FastAPI(title="xiaomi-aggregator-api", version="1.0.0")
     add_middlewares(app)
     app.include_router(api_router, prefix="/api/v1")
+    app.include_router(aggregator_router, prefix="/api/v1")
     return app
 
 
